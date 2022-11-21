20446ff7747f11d2d79b16b779c8c99bbdc3e139
PavelRossner <pavelrossner@gmail.com>
2022-11-21 22:53:00 +0100

    Aktualizace 21-11-2022

--- Pojisteni/Databaze.cs
+++ Pojisteni/Databaze.cs
@@ -20,56 +20,16 @@ namespace Pojisteni
         }
         public void Hledani(string hledaneJmeno, string hledanePrijmeni)
         {
+            var dotaz = udaje.Where(p => p.Jmeno == hledaneJmeno && p.Prijmeni == hledanePrijmeni);
 
-            /*
-
-              var dotaz = udaje.Where(p => p.Jmeno == hledaneJmeno && p.Prijmeni == hledanePrijmeni);
-
-
-              if (dotaz == null)
-              { Console.WriteLine("Osoba nebyla nalezena"); }
-
-              else
-                  foreach (var p in dotaz)
-                      Console.WriteLine("\n" + p + "\n");
-              Console.WriteLine("Pokračujte libovolnou klávesou...\n");
-              Console.ReadKey();
-            */
-
-            /*
-            var dotaz = from p in udaje
-                        where p.Jmeno == hledaneJmeno && p.Prijmeni == hledanePrijmeni
-                        select p;
             if (dotaz == null)
-                Console.WriteLine("Osoba nebyla nalezena");
+            { Console.WriteLine("Osoba nebyla nalezena"); }
 
             else
                 foreach (var p in dotaz)
                     Console.WriteLine("\n" + p + "\n");
             Console.WriteLine("Pokračujte libovolnou klávesou...\n");
             Console.ReadKey();
-            */
-
-            foreach (var p in udaje)
-                if (p.Jmeno != hledaneJmeno)
-                {
-                    Console.WriteLine("Osoba nebyla nalezena\n");
-                    Console.WriteLine("Pokračujte libovolnou klávesou...\n");
-                    Console.ReadKey();
-                }
-                else if (p.Prijmeni != hledanePrijmeni)
-                {
-                    Console.WriteLine("Osoba nebyla nalezena\n");
-                    Console.WriteLine("Pokračujte libovolnou klávesou...\n");
-                    Console.ReadKey();
-                }
-                else
-                {
-                    Console.WriteLine("\n" + p + "\n");
-                    Console.WriteLine("Pokračujte libovolnou klávesou...\n");
-                    Console.ReadKey();
-                }
-            
 
         }
         public void Vypis()
